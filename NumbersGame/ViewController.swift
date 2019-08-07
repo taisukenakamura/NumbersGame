1aa88078b071e8f2e7af7c17201b2fbbd0097acc
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-07 10:30:26 +0900

    ボタンを押した後の挙動と細かな修正

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //    入力した数字を表示するラベルの変数
-    @IBOutlet weak var printLabel: UILabel!
+    @IBOutlet weak var printScore: UILabel!
     //    入力する数字を受け取る変数
-    @IBOutlet weak var acceptedNumber: UITextField!
+    @IBOutlet weak var textNumber: UITextField!
     //    結果を表示する変数
     @IBOutlet weak var resultMessage: UITextView!
     //    乱数を持つ変数を作成
@@ -22,10 +22,62 @@ class ViewController: UIViewController {
     var counts : Int = 0
     //    Messageの初期値を設定する
     var acceptedNum = ""
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //        TextViewを編集させない
+        resultMessage.isEditable = false
+        print(randomNum)
+    }
+    
+    
+    @IBAction func tappedButton(_ sender: Any) {
+        //        数値のみだけを処理させて、他のnilやString型を処理させないようにする
+        guard let acceptedNum = Int(textNumber.text!) else {return}
+        
+        //         let acceptedNum = Int(textNumber.text!)!ß ←ゴミコードの残骸
+        if 0 < acceptedNum && acceptedNum <= 100{
+            //             入力数値　＞　正解数値
+            if acceptedNum > randomNum{
+                //                現在の回数に　＋１
+                counts += 1
+                //                入力数値をラベルに表示
+                printScore.text = "\(acceptedNum)"
+                //                不正解アラート
+                showAlert(title: "不正解",message: "答えは\(acceptedNum)より低い値です")
+                //                履歴更新
+                resultMessage.text += "[\(counts)回目]答えは\(acceptedNum)より低い値です。\n"
+                //                入力数値　＜　正解数値
+            }else if acceptedNum < randomNum {
+                //                現在の回数に　＋１
+                counts += 1
+                //                入力した数値をラベルに表示
+                printScore.text = "\(acceptedNum)"
+                //                不正解アラート
+                showAlert(title: "不正解",message: "答えは\(acceptedNum)より高い値です")
+                //                履歴更新
+                resultMessage.text += "[\(counts)回目]答えは\(acceptedNum)より高い値です。\n"
+                //                正解した場合
+            }else{
+                //                現在の回数に　＋１
+                counts += 1
+                //                正解アラート
+                showAlert(title: "正解",message: "[\(counts)回目]で正解しました。\n 数字をリセットします")
+                //                履歴更新
+                resultMessage.text +=  "[\(counts)回目]答えは\(acceptedNum)です \n"
+                //                正解したため、答えの値を更新
+                resetRandomNum()
+                //                回数を振り出しに戻す
+                counts = 0
+                
+            }
+            //            1~100以外の数字を入力した場合
+        }else{
+            showAlert(title: "エラー",message: "1~100以内の数値を入力してください")
+        }
+        
     }
     
     //    アラートを表示する、アラート関数
@@ -39,7 +91,12 @@ class ViewController: UIViewController {
         //        アラートを起動させる
         present(alert, animated: true, completion: nil)
     }
-
-
+    
+    //    正解を更新する関数
+    func resetRandomNum(){
+        randomNum = Int(arc4random_uniform(101))
+    }
+    
+    
 }
 
