5e406171b45b87f837d0c337945ea528141fc119
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-07 09:52:29 +0900

    使うかもしれない変数の定義とアラート関数の作成

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //    入力した数字を表示するラベルの変数
+    @IBOutlet weak var printScore: UILabel!
+    //    入力する数字を受け取る変数
+    @IBOutlet weak var textNumber: UITextField!
+    //    結果を表示する変数
+    @IBOutlet weak var resultMessage: UITextView!
+    //    乱数を持つ変数を作成
+    var randomNum = Int(arc4random_uniform(101))
+    //    回数を数える変数
+    var counts : Int = 0
+    //    Messageの初期値を設定する
+    var acceptedNum = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    //    アラートを表示する、アラート関数
+    func showAlert(title : String,message : String) {
+        //        アラート表示の生成
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        //        アラートからの遷移先を生成
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        //        上記二つの定数を結合
+        alert.addAction(close)
+        //        アラートを起動させる
+        present(alert, animated: true, completion: nil)
     }
 
 
